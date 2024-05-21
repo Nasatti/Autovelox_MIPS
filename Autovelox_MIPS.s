b3da975dddbb358ccd251f038b8c775a0a765f6e
Giacomo Previtali <giacomoprevitali26@gmail.com>
2024-05-21 12:05:45 +0200

    Programma finito e commentato

--- Autovelox_MIPS.s
+++ Autovelox_MIPS.s
@@ -1,6 +1,6 @@
 .data
 
-	I_O:	.half 0x0		#inizializzazione variabile a 16 bit
+	I_O:		.half 0x0		
 	
 	unsec:		.word 0x5F5E100 	#1s
 	cinqms:		.word 0x04C4B40		#50ms
@@ -9,24 +9,14 @@
 	cinquanacinque:	.word 0x63DC77 		
 	sessanta:	.word 0x5B88D0		
 
-
-	
-	#I_O
-	#15--> primo sensore
-	#14--> secondo sensore
-	#7--> otturatore
-	#1 e 0 --> risposta velocità
-
-	check1:	.half 0x8000	#controllo primo sensore linea 15
-	check2:	.half 0x4000	#controllo secondo sensore linea 14
+	check1:		.half 0x8000	#maschera primo sensore linea 15
+	check2:		.half 0x4000	#maschera secondo sensore linea 14
 	
 
 .text
 
 main:
-	add $t2, $t2, $zero		#inizializzazione contatore
 	addi $t7, $zero,1
-	add $t5, $zero,$zero		#contatore 50ms
 	lh $s1, check1
 	lh $s2, check2
 	lw $s3, cinquanta
@@ -36,20 +26,20 @@ main:
 	lw $s7, unsec
 
 
-Sensore1:
-	lh $t0, I_O			#controlla riga 15 se macchina passata
+Sensore1:				#controlla se la macchina è passata al primo sensore (Linea 15=1)
+	lh $t0, I_O			
 	and $t1, $t0, $s1
 	beq $t1, $s1, Sensore2
 	j Sensore1
 
-Sensore2:
-	lh $t0, I_O			#controlla riga 14 se macchina passata
+Sensore2:				#controlla se la macchina è passata al secondo sensore (Linea 14=1)
+	lh $t0, I_O			
 	and $t1, $t0, $s2
-	addi $t2, $t2, 4		#contatore per le 4 istruzioni precedenti
+	addi $t2, $t2, 4		
 	beq $t1, $s2, Sanzione
 	j Sensore2
 	
-Sanzione:
+Sanzione:				#controlla la velocità tramite $t2 (count) e verifica la necessità di una sanzione
 	slt $t4, $t2, $s5
 	beq $t4, $t7, II	#>60
 	slt $t4, $t2, $s4
@@ -58,9 +48,9 @@ Sanzione:
 	beq $t4, $t7, OI	#>50
 	j OO
 
-OO:
+OO:					#imposta le linee 0 e 1 in base alla velocità
 	j reset
-II:				#salviamo la velocita
+II:				
 	ori $t0, $t0, 0x3		
 	j RitornaV
 OI:
@@ -71,39 +61,33 @@ IO:
 	j RitornaV
 
 
-RitornaV:			#Ritorniamo la velosita(bit 1 e 0)
+RitornaV:				#Salva in I_O le modifiche in base alla velocità
 	sh $t0, I_O
 	j Attesa
 
-Attesa:
-	addi $t5,$t5,2			#ciclo 1 sec apri
-	bne $t5, $s7, Attesa
-	add $t5,$zero,$zero
-				
-	ori $t0, $t0, 0x80 		#mette a 1 riga 7
+Attesa:					
+	addi $t5,$t5,2			
+	bne $t5, $s7, Attesa		#Attende 1 secondo
+	add $t5,$zero,$zero			
+	ori $t0, $t0, 0x80 		#Apre otturatore (Linea 7=1)
 	sh $t0, I_O
 	j Impulso
 	
 
 
 Impulso:
-	addi $t5,$t5,3			#ciclo 50 ms impulso
+	addi $t5,$t5,3			#Attende 50ms		
 	bne $t5, $s6, Impulso
-	
-	andi $t0, $t0, 0xFF7F		#riporre 0 linea 7
+	andi $t0, $t0, 0xFF7F		#Chiude otturatore (Linea 7=0)
 	sh $t0, I_O
 	j reset
 
 
 
 
-reset:					#ripatre il ciclo
+reset:					#reset variabili e riparte
 	add $t4, $zero, $zero
+	add $t5, $zero,$zero
+	add $t2, $zero, $zero
 	sh $t4, I_O
-	j main
-
-
-#velocità quando mandarla? prima di scattare la foto
-#andi tra registro e numero? --> and come farla? or
-#fotocamera mettiamo noi con set value o da codice automaticamente
-#flow chart --> draw.io
+	j Sensore1
